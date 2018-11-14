1eb18f1f01612830dbe2b3633ba5fa7bfc5eda4e
predictly-jx <predictly-jx@predictly.se>
2018-11-14 18:31:06 +0000

    Initial import

---
+++ src/main/java/se/patpe/myartifact/DemoApplication.java
@@ -0,0 +1,12 @@
+package se.patpe.myartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
